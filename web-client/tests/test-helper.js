85e461f5f57629bea904af10b4f4073286e36a20
Ryan Gerry <gerryster@gmail.com>
2024-09-02 16:13:29 -0500

    initial Ember web client

---
+++ web-client/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'web-client/app';
+import config from 'web-client/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
